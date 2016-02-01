f31c303bd51449f5efa1e9f155324c74cc9c8df9
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 19:28:11 -0700

    :art: Cleanup BrowserifyOptions implementation

--- src/main.js
+++ src/main.js
@@ -4,50 +4,40 @@ import Path from 'path'
 import Browserify from 'browserify'
 import {Readable} from 'streams'
 
-const PrivateOptions = ['external', 'transforms', 'require', 'exclude', 'ignore', 'plugins']
+const BrowserifyOptions = ['external', 'transforms', 'require', 'exclude', 'ignore', 'plugins']
 
 export const compiler = false
 export const minifier = false
 
-export function process(contents, {root, relativePath, absolutePath, fileName}, {state, config}) {
-  const fileDir = Path.dirname(absolutePath)
+export function process(contents, {rootDirectory, filePath, config, state}) {
   const stream = new Readable()
   stream._read = function() {
     stream.push(contents)
     stream.push(null)
   }
 
-  const options = Object.assign({
-    fullPaths: true
-  }, config.browserify, {
-    basedir: fileDir,
+  const options = Object.assign(config.browserify, {
+    basedir: rootDirectory,
     debug: true,
-    filename: fileName,
-    paths: [fileDir, root]
-  })
-  const optionsPrivate = {}
-
-  PrivateOptions.forEach(function(name) {
-    if (typeof options[name] !== 'undefined') {
-      optionsPrivate[name] = options[name]
-      delete options[name]
-    } else {
-      optionsPrivate[name] = []
-    }
+    filename: Path.basename(filePath),
+    paths: [rootDirectory],
+    fullPaths: true
   })
 
   const browserified = new Browserify(options)
-
-  PrivateOptions.forEach(function(name) {
-    const options = optionsPrivate[name]
-    browserified[name](...options)
-  })
 
   return new Promise(function(resolve, reject) {
     browserified.bundle(function(error, buffer) {
       if (error) {
         reject(error)
       } else resolve(buffer.toString())
+    })
+    BrowserifyOptions.forEach(function(name) {
+      if (typeof options[name] !== 'undefined') {
+        options[name].forEach(function(options) {
+          browserified[name](...options)
+        })
+      }
     })
     browserified.pipeline.on('file', function(file){
       state.imports.push(file)
